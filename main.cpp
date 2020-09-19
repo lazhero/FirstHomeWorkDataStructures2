030e2f444b0e6929d823fa62cd2e36d87e72f7b9
lazhero <luisandreyzuniga@gmail.com>
2020-09-19 07:58:28 -0600

    Adding DoubleNode and DoubleList

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include "DoubleList/DoubleList.hpp"
+#include<string>
 
+void satanas();
+DoubleList<int> *List;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+   satanas();
+   std::cout<<*List->get(2)<<std::endl;
+    List->erase(3);
+    List->erase(9);
+    std::cout<<*List->get(2)<<std::endl;
 }
+void satanas(){
+    List=new DoubleList<int>;
+    for(int i=1;i<=5;i++){
+        List->add(i);
+    }
+   //List->erase(7);
+
+
+}
+
+
+
